4541e3cd6713e7f3daa8da717a37d58c1e95c752
Deluxurous <869398327@qq.com>
2020-10-02 08:32:45 +0800

    second attempt

--- problem-set-3/answer.hs
+++ problem-set-3/answer.hs
@@ -4,10 +4,10 @@ data Foo = Foo { left :: S.Set Foo, right :: S.Set Foo}
            deriving (Show)
 
 instance Ord Foo where
-  lhs <= rhs = ((S.lookupGT rhs (left lhs)) == Nothing) && ((S.lookupLE lhs (right rhs)) == Nothing)
+  lhs <= rhs = ((S.lookupGE rhs (left lhs)) == Nothing) && ((S.lookupLE lhs (right rhs)) == Nothing)
 
 instance Eq Foo where
-  lhs == rhs = (lhs <= rhs) && (lhs <= rhs)
+  lhs == rhs = (lhs <= rhs) && (rhs <= lhs)
 
 instance Num Foo where
   (+) lhs rhs = Foo (S.union (S.map (+ rhs) (left lhs)) (S.map (+ lhs) (left rhs))) (S.union (S.map (+ rhs) (right lhs)) (S.map (+ lhs) (right rhs)))
