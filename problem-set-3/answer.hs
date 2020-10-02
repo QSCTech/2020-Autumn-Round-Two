0a2f0c7abdda855624c27fb503117f26e878aa86
Deluxurous <869398327@qq.com>
2020-10-02 21:23:16 +0800

    1002 Update

--- problem-set-3/answer.hs
+++ problem-set-3/answer.hs
@@ -1,7 +1,7 @@
 import qualified Data.Set as S
 
 data Foo = Foo { left :: S.Set Foo, right :: S.Set Foo}
-           deriving (Show)
+           -- deriving (Show)
 
 instance Ord Foo where
   lhs <= rhs = ((S.lookupGE rhs (left lhs)) == Nothing) && ((S.lookupLE lhs (right rhs)) == Nothing)
@@ -11,3 +11,10 @@ instance Eq Foo where
 
 instance Num Foo where
   (+) lhs rhs = Foo (S.union (S.map (+ rhs) (left lhs)) (S.map (+ lhs) (left rhs))) (S.union (S.map (+ rhs) (right lhs)) (S.map (+ lhs) (right rhs)))
+
+showSF x = if null x
+           then ""
+           else (show (S.findMin x)) ++ (showSF (S.deleteMin x))
+
+instance Show Foo where
+  show x = "{" ++ (showSF (left x)) ++ "|" ++ (showSF (right x)) ++ "}"
